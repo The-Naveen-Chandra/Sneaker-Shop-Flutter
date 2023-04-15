3a872a4b160a8416d8f5d70e198d8ed352a2af18
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-15 11:51:37 +0530

    Added: Intro Page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          //logo
+
+          // title
+
+          // subtitle
+
+          // start now button
+        ],
+      ),
+    );
+  }
+}
