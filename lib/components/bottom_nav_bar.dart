fab13bf64b3fa61b3f4f84277d0d28fdad6a8a23
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-15 15:17:37 +0530

    Added: Bottom Nav Bar.

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  void Function(int)? onTabChange;
+  MyBottomNavBar({super.key, required this.onTabChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 20,
+      ),
+      child: GNav(
+        mainAxisAlignment: MainAxisAlignment.center,
+        haptic: true,
+        gap: 8.0,
+        tabBorderRadius: 16,
+        tabActiveBorder: Border.all(color: Colors.white),
+        color: Colors.grey[400],
+        activeColor: Colors.grey.shade900,
+        tabBackgroundColor: Colors.grey.shade100,
+        onTabChange: (value) => onTabChange!(value),
+        tabs: const [
+          GButton(
+            icon: Icons.home_rounded,
+            text: "Shop",
+          ),
+          GButton(
+            icon: Icons.shopping_bag_rounded,
+            text: "Cart",
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:sneaker_shop/components/bottom_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+
+  // this method will update selected index
+  // when the user taps to the bottom bar
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages to display
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+
+    // Cart page 
+    const CartPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+    );
+  }
+}
