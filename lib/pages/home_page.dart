a52476240dab890839afb4f3b9ec33158eea7ffc
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-15 17:26:15 +0530

    Created: App Drawer.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sneaker_shop/components/bottom_nav_bar.dart';
 import 'package:sneaker_shop/pages/cart_page.dart';
 import 'package:sneaker_shop/pages/shop_page.dart';
@@ -28,7 +29,7 @@ class _HomePageState extends State<HomePage> {
     // shop page
     const ShopPage(),
 
-    // Cart page 
+    // Cart page
     const CartPage(),
   ];
 
@@ -39,6 +40,149 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Builder(
+          builder: (context) => IconButton(
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: Icon(
+                Icons.menu_rounded,
+                color: Colors.grey.shade900,
+              ),
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                // logo
+                DrawerHeader(
+                  child: Image.asset(
+                    "assets/images/Nike-Logo.png",
+                    color: Colors.white,
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Divider(
+                    color: Colors.grey[800],
+                  ),
+                ),
+
+                // other apps
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.home_rounded,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      "Home",
+                      style: GoogleFonts.roboto(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.favorite,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      "Favorites",
+                      style: GoogleFonts.roboto(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.shopping_bag_rounded,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      "Cart",
+                      style: GoogleFonts.roboto(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.account_circle,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      "Account",
+                      style: GoogleFonts.roboto(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.info_outline_rounded,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      "About",
+                      style: GoogleFonts.roboto(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.ios_share,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      "Share",
+                      style: GoogleFonts.roboto(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 25.0,
+                bottom: 10.0,
+              ),
+              child: ListTile(
+                leading: const Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Logout",
+                  style: GoogleFonts.roboto(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
