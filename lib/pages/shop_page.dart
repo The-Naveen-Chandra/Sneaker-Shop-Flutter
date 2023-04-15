47a8412ac10ecf48ee52c24cc57b45dd79804476
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-15 19:35:58 +0530

    Styling the shop page.

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -10,6 +11,78 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        // search bar
+        Container(
+          padding: const EdgeInsets.all(12.0),
+          margin: const EdgeInsets.symmetric(horizontal: 25.0),
+          decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(8.0)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Search",
+                style: GoogleFonts.roboto(
+                  color: Colors.black,
+                ),
+              ),
+              const Icon(Icons.search_rounded)
+            ],
+          ),
+        ),
+
+        // message
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25),
+          child: Text(
+            "Everyone flies... some fly longer than others.",
+            style: GoogleFonts.poppins(
+              color: Colors.grey[800],
+            ),
+          ),
+        ),
+
+        // hot picks
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "Hot Picks 🔥",
+                style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0,
+                ),
+              ),
+              Text(
+                "See all",
+                style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber[800],
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        const SizedBox(
+          height: 10,
+        ),
+
+        // list of shoes
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, intdex) {
+              return ShoeTile();
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
