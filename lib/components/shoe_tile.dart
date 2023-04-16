197230f9e890ceb35b43610fff23e3500649cfd1
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-16 11:05:44 +0530

    Styling the shoe tile.

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:sneaker_shop/models/shoe_data.dart';
 
 class ShoeTile extends StatelessWidget {
-  // Shoe shoe;
-  ShoeTile({super.key, });
+  Shoe shoe;
+  ShoeTile({super.key, required this.shoe});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,17 @@ class ShoeTile extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
       ),
       child: Column(children: [
-        //
+        // shoe image
+        Image.asset(shoe.imgPath)
+
+
+        // description
+
+
+        // price + details
+
+        
+        // button to add to cart
 
       ]),
     );
