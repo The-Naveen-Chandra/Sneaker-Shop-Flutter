58ecf63279fc0a681ba9dc38edb8ac9d1df470d3
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-17 16:07:38 +0530

    Added: Remove function.

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sneaker_shop/components/cart_item.dart';
+import 'package:sneaker_shop/models/cart_model.dart';
+import 'package:sneaker_shop/models/shoe_data.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,8 +15,44 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("CART PAGE"),
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // heading
+            Text(
+              "My Cart",
+              style: GoogleFonts.roboto(
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.getUserCart().length,
+                itemBuilder: (context, index) {
+                  // get individual shoe
+                  Shoe individualShoe = value.getUserCart()[index];
+
+                  // return the cart item
+                  return CartItem(
+                    shoe: individualShoe,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
