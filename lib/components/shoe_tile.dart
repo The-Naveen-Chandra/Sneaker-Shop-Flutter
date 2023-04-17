daabd63684ab25e895a53cf136003f9ba4b6a36c
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-17 15:36:29 +0530

    Done some styling and fix bugs.

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -4,7 +4,8 @@ import 'package:sneaker_shop/models/shoe_data.dart';
 
 class ShoeTile extends StatelessWidget {
   Shoe shoe;
-  ShoeTile({super.key, required this.shoe});
+  void Function()? onTap;
+  ShoeTile({super.key, required this.shoe, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +26,13 @@ class ShoeTile extends StatelessWidget {
           ),
 
           // description
-          Text(
-            shoe.description,
-            style: GoogleFonts.poppins(
-              color: Colors.grey[600],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              shoe.description,
+              style: GoogleFonts.poppins(
+                color: Colors.grey[600],
+              ),
             ),
           ),
 
@@ -66,18 +70,21 @@ class ShoeTile extends StatelessWidget {
                 ),
 
                 // plus button
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      bottomRight: Radius.circular(12),
+                GestureDetector(
+                  onTap: onTap,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        bottomRight: Radius.circular(12),
+                      ),
                     ),
-                  ),
-                  child: const Icon(
-                    Icons.add_rounded,
-                    color: Colors.white,
+                    child: const Icon(
+                      Icons.add_rounded,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ],
